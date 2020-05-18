67e33a4e78f5bf62e61a6a29b3d0fb03a27e67aa
Robin Wang <510471194@qq.com>
2020-05-18 17:35:58 +0800

    update

--- src/com/legend/jvm/c0_basic/TT.java
+++ src/com/legend/jvm/c0_basic/TT.java
@@ -1,5 +1,16 @@
 package com.legend.jvm.c0_basic;
 
+/*
+* 通过bytecode查看可知main的底层指令如下
+    0 new #3 <com/legend/jvm/c0_basic/TT>
+    3 dup
+    4 invokespecial #4 <com/legend/jvm/c0_basic/TT.<init>> //执行构造方法进行初始化
+    7 astore_1 //将对象的地址赋给符号引用
+    8 return
+* 4与7可能发生指令重排序，这也是为什么如果DCL中instance属性如果不加volatile会取到半初始化对象的原因
+*
+* */
+
 public class TT {
     int m = 8;
 
--- src/com/legend/jvm/c2_classloader/T001_ClassLoadingProcedure.java
+++ src/com/legend/jvm/c2_classloader/T001_ClassLoadingProcedure.java
@@ -1,5 +1,17 @@
 package com.legend.jvm.c2_classloader;
 
+/*
+* 请问交换15,16行的代码，输出的结果是什么？
+* 1. 主要看准备与初始化阶段
+*    准备阶段：静态变量赋默认值
+*    初始化阶段：静态变量赋初始值
+* 2. 初始化时从上到下按顺序进行
+*
+* 类似的在new对象的时候也分为两步
+* 1. 为对象分配内存赋初始值
+* 2. 给成员变量赋初始值
+* */
+
 public class T001_ClassLoadingProcedure {
     public static void main(String[] args) {
         System.out.println(T.count);
