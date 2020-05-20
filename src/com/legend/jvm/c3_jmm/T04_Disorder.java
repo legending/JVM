a11206a2a2dca06f7abb401a72e8b4b4ce711ac3
Robin Wang <1029126902@qq.com>
2020-05-20 17:49:06 +0800

    add annotations for for T04_Disorder.java

--- src/com/legend/jvm/c3_jmm/T04_Disorder.java
+++ src/com/legend/jvm/c3_jmm/T04_Disorder.java
@@ -1,8 +1,19 @@
 package com.legend.jvm.c3_jmm;
 
+/*
+* 证明CPU存在乱序执行的情况
+* a = 1; 与 x = b; 之间没有依赖关系
+* b = 1; 与 y = a; 之间没有依赖关系
+* 因为两个线程都被join过了，所以当执行到
+* String result = "第" + i + "次 (" + x + "," + y + "）";
+* 时，两个线程都已执行完毕，但无论哪个线程先执行完，都不可能出现x=0,y=0的情况
+*
+* 当给a/b/x/y加上volatile后问题不再出现（至少测了N久从来没有出现）
+* */
+
 public class T04_Disorder {
-    private static int x = 0, y = 0;
-    private static int a = 0, b =0;
+    private /*volatile*/ static int x = 0, y = 0;
+    private /*volatile*/ static int a = 0, b =0;
 
     public static void main(String[] args) throws InterruptedException {
         int i = 0;
